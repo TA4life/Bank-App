058b3297f7aa0c4fe57c37cdb5f21188dec63f32
Obaida <15270128+TA4life@users.noreply.github.com>
2019-07-07 21:54:02 -0400

    Add files via upload

---
+++ BankApp.java
@@ -0,0 +1,16 @@
+public class BankApp {
+    public Bank bank;
+    /** Creates a new instance of BankApp */
+    public BankApp() {
+        bank = new Bank();
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        BankApp bankApp = new BankApp();
+        BankGUI gui = new BankGUI();
+        gui.processCommands(bankApp.bank);
+    }
+}
